27d9c19341011a1c69ad94b400cf9225637e8db2
surajbond <sbondage95@gmail.com>
2022-12-28 22:13:27 +0530

    first commit:added arraylist demo code

---
+++ src/com/practicejava/ArraylistDemo.java
@@ -0,0 +1,16 @@
+package com.practicejava;
+
+import java.util.ArrayList;
+
+public class ArraylistDemo {
+	public static void main(String[] args) {
+		ArrayList<String> list=new ArrayList<String>();
+		list.add("suraj");
+		list.add("teju");
+		list.add("bondage");
+		System.out.println(list);
+		
+		
+	}
+
+}
