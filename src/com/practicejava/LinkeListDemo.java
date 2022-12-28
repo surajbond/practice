b66332a0d90efe39b76dfdc263852fe0d976e970
surajbond <sbondage95@gmail.com>
2022-12-28 22:25:30 +0530

    second commit :added code for linkedlist

---
+++ src/com/practicejava/LinkeListDemo.java
@@ -0,0 +1,13 @@
+package com.practicejava;
+
+import java.util.LinkedList;
+
+public class LinkeListDemo {
+public static void main(String[] args) {
+	LinkedList<Integer> ls=new LinkedList<Integer>();
+	ls.add(1);
+	ls.add(2);
+	ls.add(3);
+	System.out.println(ls);
+}
+}
